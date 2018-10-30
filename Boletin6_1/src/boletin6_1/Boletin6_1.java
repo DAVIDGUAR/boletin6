a8ab0ff1ae6eea6dcf7ff80f4812b99cca7606a3
drodriguezguardiola <drodriguezguardiola@nautilus19.danielcastelao.org>
2018-10-30 12:03:05 +0100

    1

---
+++ Boletin6_1/src/boletin6_1/Boletin6_1.java
@@ -0,0 +1,16 @@
+package boletin6_1;
+
+/**
+ *
+ * @author drodriguezguardiola
+ */
+public class Boletin6_1 {
+
+ 
+    public static void main(String[] args) {
+        
+        Numero num = new Numero();
+        num.numeroPositivo();
+    }
+    
+}
