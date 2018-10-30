63d03720c847ca273327cd3fdbdcf160ea4f7b94
drodriguezguardiola <drodriguezguardiola@nautilus19.danielcastelao.org>
2018-10-30 12:35:20 +0100

    1

---
+++ boletin6_2/src/boletin6_2/Boletin6_2.java
@@ -0,0 +1,19 @@
+package boletin6_2;
+
+/**
+ *
+ * @author drodriguezguardiola
+ */
+public class Boletin6_2 {
+
+ 
+    public static void main(String[] args) {
+        
+        Operacion jthf = new Operacion();
+        jthf.sumaResta();
+        
+        System.out.println("suma="+jthf.suma);
+      
+    }
+    
+}
