755700fd820840cd7cfe0cb20329f61ee282e068
drodriguezguardiola <drodriguezguardiola@nautilus19.danielcastelao.org>
2018-11-05 11:39:24 +0100

    1

---
+++ boletin6_3/src/boletin6_3/Singno.java
@@ -0,0 +1,29 @@
+package boletin6_3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author drodriguezguardiola
+ */
+public class Singno {
+    Scanner ler = new Scanner(System.in);
+    
+    public void operacion(){
+        
+    int num1;
+    System.out.println("tecle numero");
+    num1=ler.nextInt();
+    
+    if (num1>0){
+        System.out.println("+");
+    } 
+        else if (num1<0){
+                System.out.println("-");
+                }
+    
+        else 
+            System.out.println("0");
+    }
+       
+}
