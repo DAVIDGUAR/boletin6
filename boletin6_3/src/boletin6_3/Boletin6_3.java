b2c942d332d5efcdd26b090f01897e0db6173e29
drodriguezguardiola <drodriguezguardiola@nautilus19.danielcastelao.org>
2018-11-05 11:38:57 +0100

    1

---
+++ boletin6_3/src/boletin6_3/Boletin6_3.java
@@ -0,0 +1,19 @@
+package boletin6_3;
+
+/**
+ *
+ * @author drodriguezguardiola
+ */
+public class Boletin6_3 {
+
+
+    public static void main(String[] args) {
+        
+       Singno abc = new Singno();
+       abc.operacion();
+       
+       /*System.out.println("valor numero = "+operacion);8*/
+       
+    }
+    
+}
