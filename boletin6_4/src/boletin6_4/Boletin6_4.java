3c504df71150425b4ccaae71d06e7868aba05e17
david <david@DESKTOP-AUJP76H>
2018-11-14 21:19:59 +0100

    1

--- boletin6_4/src/boletin6_4/Boletin6_4.java
+++ boletin6_4/src/boletin6_4/Boletin6_4.java
@@ -4,14 +4,32 @@ package boletin6_4;
  *
  * @author drodriguezguardiola
  */
-public class Boletin6_4 {
+public class Boletin6_4 
+{
 
   
-    public static void main(String[] args) {
-      
-        Persona persona1 = new Persona();
-        Persona persona2 = new Persona();
-        
-    }
+    public static void main(String[] args) 
     
+    {
+      
+        Persona persona1 = new Persona(75,"daniel");
+        Persona persona2 = new Persona(99,"juan");
+        
+        
+        if(persona1.getPeso() > persona2.getPeso())
+        
+        {
+            System.out.println(persona1.getNombre() + " el mas pesado es " + persona2.getNombre());
+            System.out.println("La diferencia de kg es de " + (persona1.getPeso() - persona2.getPeso()));
+        }
+        
+        else
+        
+        
+        {
+            System.out.println(persona2.getNombre() + " el mas pesado es " + persona1.getNombre());
+            System.out.println("La diferencia de kg es de " + (persona2.getPeso() - persona1.getPeso()));
+        }
+    
+    }
 }
--- boletin6_4/src/boletin6_4/Persona.java
+++ boletin6_4/src/boletin6_4/Persona.java
@@ -1,14 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package boletin6_4;
 
 /**
  *
  * @author drodriguezguardiola
  */
-public class Persona {
+public class Persona 
+{
+    
+    private final String nombre;
+    private final float peso;
+    
+    
+    public Persona(int kg, String nom)
+    
+    {
+        this.peso=kg;
+        this.nombre=nom;
+        
+    }
+    
+    
+        
+        public float getPeso()
+        
+        {
+            return peso;
+        }
+        
+        
+        public String getNombre()
+        
+        {
+            
+           return nombre; 
+        }
+        
+      /** public void diferenciaPeso()
+      
+      { 
+        int diferenciaPeso;
+        
+       diferenciaPeso=persona1.getPeso()-persona2.getPeso();
+    }
+    * 
+    * 
+    * 
+    
+    if (persona1.getPeso() < pesona2.getPeso(){
+    
+    System.out.println();
+        
+    
+    
+    
+    
+    */
+        
+         
     
 }
